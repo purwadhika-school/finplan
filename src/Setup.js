a43d98287efa3e885479ae4183865c8b2d88e472
yogie.putra8 <yogie.putra8@gmail.com>
2018-04-17 21:59:39 +0700

    my fist commit

---
+++ src/Setup.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { StyleSheet, ActivityIndicator, Text, View } from "react-native";
+import { connect } from "react-redux";
+import { fetchData } from "./actions";
+import { StackNavigator } from "react-navigation";
+import Login from "./screens/login";
+import Register from "./screens/register";
+import Home from "./screens/home";
+import IncomeList from "./screens/income_list";
+import IncomeAdd from "./screens/income_add";
+import ExpenseList from "./screens/expense_list";
+import ExpenseAdd from "./screens/expense_add";
+
+class Setup extends Component {
+  render() {
+    console.log(this.props);
+    return <Login navigation={this.props.navigation} />;
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    LoginPage: {
+      screen: Setup,
+      headerMode: "none",
+      header: null,
+      navigationOptions: {
+        header: null
+      }
+    },
+    RegisterPage: {
+      screen: Register,
+      headerMode: "none",
+      header: null,
+      navigationOptions: {
+        header: null
+      }
+    },
+    HomePage: {
+      screen: Home,
+      headerMode: "none",
+      header: null,
+      navigationOptions: {
+        header: null
+      }
+    },
+    IncomeListPage: {
+      screen: IncomeList
+    },
+    IncomeAddPage: {
+      screen: IncomeAdd
+    },
+    ExpenseListPage: {
+      screen: ExpenseList
+    },
+    ExpenseAddPage: {
+      screen: ExpenseAdd
+    }
+  },
+  {
+    initialRouteName: "LoginPage"
+  }
+);
+
+export default connect()(RootStack);
---
+++ src/screens/income_list.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import { View, FlatList, Text, TouchableOpacity } from "react-native";
+import numeral from "numeral";
+
+class IncomeList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      income_data: [
+        {
+          type: "Regular",
+          label: "Penghasilan utama",
+          amount: "5000000",
+          paydate: "1 January 2018",
+          from: "Tokopedia"
+        },
+        {
+          type: "Others",
+          label: "Penghasilan tambahan",
+          amount: "1000000",
+          paydate: "5 January 2018",
+          from: "Freelance"
+        }
+      ]
+    };
+  }
+
+  renderBody(item_saya) {
+    const amount_formatted = numeral(item_saya.item.amount).format("0,0");
+    return (
+      <View
+        style={{
+          backgroundColor: "white",
+          borderRadius: 8,
+          marginLeft: 20,
+          marginRight: 20,
+          marginBottom: 20
+        }}
+      >
+        <View style={{ flexDirection: "row", marginLeft: 20, marginTop: 10 }}>
+          <Text style={{ fontSize: 20 }}>{item_saya.item.label}</Text>
+          <View
+            style={{
+              marginLeft: 15,
+              backgroundColor: "green",
+              borderRadius: 3,
+              width: "15%"
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontWeight: "500",
+                margin: 5
+              }}
+            >
+              {item_saya.item.type}
+            </Text>
+          </View>
+        </View>
+        <Text style={{ fontSize: 40, fontWeight: "600", marginLeft: 20 }}>
+          IDR {amount_formatted}
+        </Text>
+        <Text style={{ fontSize: 15, marginLeft: 20, marginBottom: 20 }}>
+          from <Text style={{ fontWeight: "700" }}>{item_saya.item.from}</Text>
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={{ backgroundColor: "#F1F1F1", flex: 1 }}>
+        <View style={{ marginTop: 15 }}>
+          <FlatList
+            data={this.state.income_data}
+            keyExtractor={(data, index) => index}
+            renderItem={this.renderBody}
+          />
+          <TouchableOpacity
+            style={{
+              alignSelf: "center",
+              width: "40%",
+              borderColor: "#F1F1F1",
+              borderRadius: 3,
+              backgroundColor: "#77dd77",
+              borderWidth: 1,
+              marginRight: 5,
+              marginTop: 10
+            }}
+          >
+            <Text style={{ textAlign: "center", fontSize: 20, margin: 10 }}>
+              Add Income
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+export default IncomeList;
